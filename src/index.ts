b560acb306a2d65abfe5f71689025128f47e3d05
Neamix <abdalrhmanhussin44@gmail.com>
2026-07-30 18:51:10 +0300

    add no found string

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import express from 'express';
-import type { Express } from 'express';
+import type { Express,Request, Response } from 'express';
 import { userRouter } from './modules/users/user.routes.js';
-import type { Request, Response } from "express";
 import cors from "cors";
 
 const port:number = 8000;
 const app:Express = express();
 
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
 app.get('/health',(req: Request,res: Response):void => {
     res.status(201).json({
@@ -18,11 +17,9 @@ app.get('/health',(req: Request,res: Response):void => {
 
 app.use('/api/users', userRouter);
 
-app.use((_req: Request, res: Response): void => {
-    res.status(404).json({
-        message: "No route found",
-    });
-});
+app.use((req: Request,res: Response) => {
+    res.status(404).json("No route found")
+})
 
 app.listen(port,():void => {
     console.log("Listening on port: ",port);
