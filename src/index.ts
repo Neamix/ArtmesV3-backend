644bfcdac9687fc7da22dcdc661d92e6952c1903
Neamix <abdalrhmanhussin44@gmail.com>
2026-07-30 18:50:38 +0300

    adding index

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,29 @@
 import express from 'express';
 import type { Express } from 'express';
+import { userRouter } from './modules/users/user.routes.js';
+import type { Request, Response } from "express";
+import cors from "cors";
 
 const port:number = 8000;
 const app:Express = express();
 
+app.use(cors());
 app.use(express.json());
 
-app.listen(port,() => {
-    console.log("Listening on port: ",port);
+app.get('/health',(req: Request,res: Response):void => {
+    res.status(201).json({
+        data: 'test server is live'
+    })
 })
+
+app.use('/api/users', userRouter);
+
+app.use((_req: Request, res: Response): void => {
+    res.status(404).json({
+        message: "No route found",
+    });
+});
+
+app.listen(port,():void => {
+    console.log("Listening on port: ",port);
+});
